fb5c0076cbcd783aaef14d04bc50892968d44261
ReeceRenninger <renningerreece@gmail.com>
2023-06-28 10:38:27 -0700

    fixed pagniation issue

--- src/Components/List/index.jsx
+++ src/Components/List/index.jsx
@@ -4,17 +4,17 @@ import { useContext, useState } from 'react';
 
 import { Grid, Card } from '@mantine/core';
 
-function List(props) {
+function List({list, toggleComplete}) {
 
   const { pageItems, showCompleted, sort } = useContext(SettingsContext);
   const [currentPage, setCurrentPage] = useState(1);
 
-  const pages = Math.ceil(props.list.length / pageItems);
-
   const displayItems = showCompleted 
-  ? props.list
-  : props.list.filter((items) => !items.complete);
-
+  ? list
+  : list.filter((items) => !items.complete);
+  
+  
+  const pages = Math.ceil(displayItems.length / pageItems);
   const firstItem = (currentPage - 1) * pageItems;
   const lastItem = currentPage * pageItems;
   const finalItems = displayItems.slice(firstItem, lastItem);
@@ -29,7 +29,7 @@ function List(props) {
           <p>{item.text}</p>
           <p><small>Assigned to: {item.assignee}</small></p>
           <p><small>Difficulty: {item.difficulty}</small></p>
-          <div onClick={() => props.toggleComplete(item.id)}>Complete: {item.complete.toString()}</div>
+          <div onClick={() => toggleComplete(item.id)}>Complete: {item.complete.toString()}</div>
           <hr />
         </div>
       ))}
