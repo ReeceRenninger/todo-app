d741bebeddd7fd0e1f4d2bcdba7dfeb923111d7f
ReeceRenninger <renningerreece@gmail.com>
2023-06-28 08:03:15 -0700

    nav bar added to both routes for easy transition

--- src/Components/SettingsForm/index.jsx
+++ src/Components/SettingsForm/index.jsx
@@ -1,11 +1,32 @@
 import { useContext } from "react";
 import { SettingsContext } from "../Context/Settings";
 
+import { createStyles, Header, Navbar } from "@mantine/core";
+import { Link } from "react-router-dom";
+
+const useStyles = createStyles((theme) => ({
+  nav: {
+    backgroundColor: theme.colors.blue[6],
+    height: '100%',
+    margin: 'auto',
+    color: 'white',
+    display: 'flex',
+    paddingBottom: '15px',
+  }
+}));
+
 const SettingsForm = (event) => {
+  const { classes } = useStyles();
   const { pageItems, setPageItems, completed, setCompleted, sort, setSort } = useContext(SettingsContext);
 
   return (
     <>
+       <Header >
+      <Navbar className={classes.nav}>
+        <Link to='/'>Home</Link>
+        <Link to='/settings'>Settings</Link>
+      </Navbar>
+    </Header>
     <h1> Manage Settings </h1>
       <form>
         <label>
